99ec0eee85adf7b2ceb8d56cafcc0f215c939447
signakada <signakada@mac.com>
2025-09-02 23:26:06 +0900

    Delete VoiceToDocLite directory

--- VoiceToDocLite/AppCore/Notifications.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-// アプリ内で共有する通知名を一元管理
-extension Notification.Name {
-    /// 録音（または外部処理）でWAV保存が完了し、ファイルURLが用意できたとき
-    static let audioFileReady          = Notification.Name("audioFileReady")
-}
